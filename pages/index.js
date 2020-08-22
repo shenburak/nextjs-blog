91a8e1f2c0ea667ecd8a09c69b1d22298cf31604
Burak Shen <tobuba2@gmail.com>
2020-08-22 15:14:47 +0300

    previewe update string

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>ben benim siz sizsiniz xd develop</p>
+        <p>ben benim siz sizsiniz xd preview</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
